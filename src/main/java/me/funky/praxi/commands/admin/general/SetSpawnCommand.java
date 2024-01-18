4cc32388a3323af6c87c1202b0c98481c34d17ae
funkyranveer <ranveerkochhar09@gmail.com>
2024-01-18 11:51:36 +1100

    Initial commit

---
+++ src/main/java/me/funky/praxi/commands/admin/general/SetSpawnCommand.java
@@ -0,0 +1,16 @@
+package me.funky.praxi.commands.admin.general;
+
+import me.funky.praxi.util.command.command.CPL;
+import me.funky.praxi.util.command.command.CommandMeta;
+import me.funky.praxi.Praxi;
+import me.funky.praxi.util.CC;
+import org.bukkit.entity.Player;
+
+@CommandMeta(label = { "setspawn" }, permission = "praxi.setspawn")
+public class SetSpawnCommand
+{
+    public void execute(Player player) {
+        Praxi.get().getEssentials().setSpawn(player.getLocation());
+        player.sendMessage(CC.translate("&bSpawn set successfully!"));
+    }
+}
---
+++ src/main/java/me/funky/praxi/commands/event/admin/EventHelpCommand.java
@@ -0,0 +1,18 @@
+package me.funky.praxi.commands.event.admin;
+
+import me.funky.praxi.Praxi;
+import me.funky.praxi.util.command.command.CPL;
+import me.funky.praxi.util.command.command.CommandMeta;
+import me.funky.praxi.util.CC;
+import org.bukkit.entity.Player;
+
+@CommandMeta(label = { "event", "event help" })
+public class EventHelpCommand {
+
+	public void execute(Player player) {
+		for (String line : Praxi.get().getMainConfig().getStringList("EVENT.HELP")) {
+			player.sendMessage(CC.translate(line));
+		}
+	}
+
+}
